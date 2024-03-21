c49e109101059c5a4c6cd7adb3c8dbd0d4a14e68
AndreR114 <129870218+AndreR114@users.noreply.github.com>
2024-03-21 13:20:56 +0100

    Add files via upload
    Änderungen an der Navigation, Logoanpassung, Transformationseffekte eingefügt

---
+++ MusikvereinJS.js
@@ -0,0 +1,30 @@
+//Aus und einblenden des Banners beim scrollen
+
+window.onscroll = function () {
+  scrollFunction();
+  stickNavigation();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("banner").style.opacity = "0";
+    document.getElementById("banner").style.visibility = "hidden";
+  } else {
+    document.getElementById("banner").style.opacity = "1";
+    document.getElementById("banner").style.visibility = "visible";
+  }
+}
+
+//Navigationsleist oben anpinnen und besser sichtbar machen
+
+function stickNavigation() {
+  var navigation = document.getElementById("navigation");
+  // Ersetzen Sie "20" durch die tatsächliche Höhe Ihres Banners, wenn bekannt
+  if (window.scrollY > 60) {
+    navigation.classList.add("sticky-nav");
+    navigation.style.transform = "translateY(0%)";
+  } else {
+    navigation.classList.remove("sticky-nav");
+    navigation.style.transform = "translateY(-100%)";
+  }
+}
